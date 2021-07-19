d25a998ca8056f5bcd5d250cd5a522b593a9d2d6
simon <parkseungchul@gmail.com>
2021-07-19 22:52:34 +0900

    first

---
+++ R101/src/main/java/com/psc/sample/r101/R101Application.java
@@ -0,0 +1,13 @@
+package com.psc.sample.r101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R101Application.class, args);
+    }
+
+}
---
+++ R101/src/test/java/com/psc/sample/r101/R101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psc.sample.r101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
