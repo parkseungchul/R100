e53aac720455d0dcd463024f7b9f1725bb60d5fe
simon <parkseungchul@gmail.com>
2022-01-27 21:43:52 +0900

    script

--- R102/docker_script/004_RUN_BATCH.sh
+++ R102/docker_script/004_RUN_BATCH.sh
@@ -1,5 +1,5 @@
 docker run -itd --name docker_batch_app \
     --network docker_batch_network \
     -e SPRING_PROFILES_ACTIVE=docker \
-    -v ../INFILES/data:/data \
+    -v /APP/R100/R102/INFILES:/INFILES \
     docker_batch_app_image
